0383e17bd9b7d7c25fd66354870cf8facef97380
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-03-20 12:38:05 +0900

    UPDATE AuthControllerTest

--- src/test/java/com/example/jats/domain/AuthControllerTest.java
+++ src/test/java/com/example/jats/domain/AuthControllerTest.java
@@ -5,14 +5,17 @@ import com.example.jats.entity.user.User;
 import com.example.jats.entity.user.UserRepository;
 import com.example.jats.entity.user.enums.Region;
 import com.example.jats.payload.request.SignInRequest;
+import com.example.jats.payload.response.TokenResponse;
 import com.example.jats.security.JwtTokenProvider;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,6 +23,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -30,6 +34,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = JatsApplication.class)
 @ActiveProfiles({"test"})
 public class AuthControllerTest {
+
+    @Value("${auth.jwt.secret}")
+    private String secret;
 
     @Autowired
     private WebApplicationContext context;
@@ -73,10 +80,14 @@ public class AuthControllerTest {
     void signInTest() throws Exception {
         SignInRequest request = new SignInRequest("id","pwd");
 
-        mockMvc.perform(post("/auth")
+        MvcResult result = mockMvc.perform(post("/auth")
                 .content(new ObjectMapper().writeValueAsString(request))
                 .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andReturn();
+        Assertions.assertEquals(jwtTokenProvider.validateToken
+                (new ObjectMapper().readValue(result.getResponse().getContentAsString(), TokenResponse.class).getAccessToken()), true);
+
     }
 
     @Test
